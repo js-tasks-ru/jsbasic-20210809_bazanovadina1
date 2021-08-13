8cd8c5496ea81b104b7c20b0c1066a1238b8f4a5
Dina Bazanova <bazanovadina1@gmail.com>
2021-08-13 22:35:10 +0300

    Решение задачи 1, модуль 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  let result
+  
+  if (n > 1) {
+    result = n
+    do { result *= (--n)
+    } while (n > 1)
+  } else {
+    result = 1
+  }
+  
+return result
 }

8fd8f8a0890324102b870940420130c92511c6fa
Dina Bazanova <bazanovadina1@gmail.com>
2021-08-17 11:56:24 +0300

    Решение задачи 2, модуль 1

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -9,8 +9,12 @@ function print(text) {
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
-function isValid(name) {
-  // ваш код...
+ function isValid(name) {
+  if (name !== (undefined || null) && name.length >= 4 && !name.includes(" ")) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function sayHello() {
